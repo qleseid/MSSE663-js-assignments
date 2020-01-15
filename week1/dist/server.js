d9581fa575aade2523637b201a96bd82fe7ce82b
Lucas <qleseid@yahoo.com>
2020-01-15 00:24:33 -0700

    Added ESLint as a dev dependency

--- week1/dist/server.js
+++ week1/dist/server.js
@@ -1,11 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var app = express();
-function serve(req, res) {
-    res.send('Sup Man!');
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
+var express = require('express')
+var app = express()
+function serve (req, res) {
+  res.send('Sup Man!')
 }
-app.use('/', serve);
+app.use('/', serve)
 app.listen(8000, function () {
-    console.log('Server Running....');
-});
+  console.log('Server Running....')
+})

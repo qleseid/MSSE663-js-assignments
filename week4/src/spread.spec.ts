c276fbf48c9ad8e58c5f4b5c8a320630d172c1ce
Lucas <qleseid@yahoo.com>
2020-02-08 06:11:21 -0700

    completed Assignment #4

--- week4/src/spread.spec.ts
+++ week4/src/spread.spec.ts
@@ -1,2 +1,32 @@
 import { expect } from 'chai';
 import 'mocha';
+
+import { 
+    newNum, 
+    originalNumbers, 
+    newWin, 
+    originalContenders, 
+    newPerson,
+    newUp,
+    newData,
+    update,
+    originalPersons,
+    originalUpdates} from "./spread";
+
+describe("Week 4 assignments", () => {
+  it("should output array [1,2,3,4,5]", () => {
+    expect(newNum).to.be.eql(originalNumbers);
+  });
+
+  it("should output a number 5", () => {
+    expect(newWin.length).to.be.eql(originalContenders);
+  });
+
+  it("should output objects { man: 'Bob', boy: 'Kyle' }", () => {
+    expect(newPerson).to.be.eql(originalPersons);
+  });
+
+  it("should output modified #1 object", () => {
+    expect(newUp(update, ...newData)).to.be.eql(originalUpdates);
+  });
+});
--- week4/src/spread.ts
+++ week4/src/spread.ts
@@ -5,15 +5,23 @@ export const originalNumbers = numbers.map(number => number);
 console.log(originalNumbers);
 
 // refactor here
+export const nums = [...numbers];
+export const newNum = nums.map(nun => nun);
+console.log(newNum);
 
 // #2 Combining arrays
 export const winners = ['first'];
 export const runnerUps = ['second', 'third', 'fourth', 'fifth'];
+// console.log(winners.unshift(runnerUps[1]));
+// console.log(runnerUps.unshift);
 
+export let newRunner = [...runnerUps];
 export const originalContenders = runnerUps.unshift(winners[0]);
-console.log(originalContenders);
+console.log(typeof originalContenders);
 
 // refactor here
+export const newWin = ['first', ...newRunner];
+console.log(typeof newWin);
 
 // #3 Combining objects
 export const bob = {
@@ -25,8 +33,11 @@ export const kyle = {
 };
 
 export const originalPersons = Object.assign(bob, kyle);
+console.log(originalPersons);
 
 // refactor here
+export const newPerson = {...bob, ...kyle};
+console.log(newPerson);
 
 // #4 Modifying values in arrays of objects
 export const data = [
@@ -49,6 +60,13 @@ export const update = {
   task: 'Do the other thing... again'
 };
 
+
+export let newData = JSON.parse(JSON.stringify(data));
+/*
+console.log(newData);
+console.log(data);
+*/
+
 export const originalUpdates = data.map(task => {
   if (task.id === update.id) {
     return Object.assign(task, update);
@@ -56,4 +74,15 @@ export const originalUpdates = data.map(task => {
   return task;
 });
 
+console.log(originalUpdates);
+
 // refactor here
+export const newUp = (up: any, ...dat: any) =>
+{
+  dat[up.id] = up;
+  return dat;
+}
+
+// console.log(newData);
+
+console.log(newUp(update, ...newData));
